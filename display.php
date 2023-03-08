0cdb46b98e88e57c805da3db139f302a85fe7c3b
Khiro Sasha <sasha.khiro@gmail.com>
2023-03-08 18:29:40 +0200

    changed logic to send response

--- display.php
+++ display.php
@@ -1,40 +1,43 @@
 <?php
-    $count = $_POST['count'];
-    $dice = $_POST['dice'];
-    
-    if($count == null || $dice == null )
-        echo "<h1>Fail with data!</h1>";
+$data = json_decode(file_get_contents('php://input'), true);
 
-    $results = [];
+$count = $data['count'];
+$dice = $data['dice'];
 
-    for($i = 0; $i < $count; $i++){
-        switch($dice){
-            case "d4":
-                $results[] = rand(1, 4);
-                break;
-            case "d6":
-                $results[] = rand(1, 6);
-                break;
-            case "d8":
-                $results[] = rand(1, 8);
-                break;
-            case "d10":
-                $results[] = rand(1, 10);
-                break;
-            case "d12":
-                $results[] = rand(1, 12);
-                break;
-            case "d20":
-                $results[] = rand(1, 20);
-                break;
-        } 
-    }
+$results = [];
 
-    echo ('Rolled '. $count . ' ' . strtoupper($dice) . ': ' . array_sum($results) .' | Rolls: ');
-    foreach($results as $key => $value){
-        if($key == count($results) - 1)
-            echo(' '. $value . '');
-        else
-            echo(''. $value . ', ');
-    }
-?>
+for ($i = 0; $i < $count; $i++) {
+  switch ($dice) {
+    case "d4":
+      $results[] = rand(1, 4);
+      break;
+    case "d6":
+      $results[] = rand(1, 6);
+      break;
+    case "d8":
+      $results[] = rand(1, 8);
+      break;
+    case "d10":
+      $results[] = rand(1, 10);
+      break;
+    case "d12":
+      $results[] = rand(1, 12);
+      break;
+    case "d20":
+      $results[] = rand(1, 20);
+      break;
+  }
+}
+
+if(count($results) > 0 )
+{
+  $arrayString = implode(', ', $results);
+  $json = json_encode("Rolled $count " .strtoupper($dice) .": " . array_sum($results) ." | Rolls: $arrayString", JSON_PRETTY_PRINT);
+}
+else
+$json = json_encode("Number of Dice not correct", JSON_PRETTY_PRINT);
+
+
+echo $json;
+
+//alt + shif + f
